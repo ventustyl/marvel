f7f48d155219fe5a09a72101e2a60450335d395f
ventustyl <eric.venturino@orange.fr>
2022-09-19 09:40:27 +0200

    envoi 2

---
+++ script.js
@@ -0,0 +1,38 @@
+
+        gsap.from("#widow",{
+            scrollTrigger : {
+                scrub: true
+            },
+            y:-2000,duration: 2
+        })
+        gsap.from("#marvel",{
+            scrollTrigger : {
+                scrub: true
+            },
+            y:1000, duration: 2
+        })
+        gsap.from("#thor",{
+            scrollTrigger : {
+                scrub: true
+            },
+            y:5000,
+            x: 5000, duration: 3
+        })
+        gsap.from("#iron",{
+            scrollTrigger : {
+                scrub: true
+            },
+            y:-2500, duration: 4
+        })
+        gsap.from("#hulk",{
+            scrollTrigger : {
+                scrub: true
+            },
+            y:2500, x:-1000, duration: 4
+        })
+        gsap.from("#pool",{
+            scrollTrigger : {
+                scrub: true
+            },
+            x:-2500, duration: 6
+        })
